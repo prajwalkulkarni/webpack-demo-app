e31cbd1c7d4b860e145db948ca1316f85da4b928
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-05-05 19:31:57 +0530

    Added cache busting and html webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode:'development',
     entry:'./src/index.js',
     output:{
-        filename:'main.js',
+        filename:'main.[contenthash].js',
         path: path.join(__dirname,'dist')
     },
     module:{
@@ -14,5 +15,8 @@ module.exports = {
             use:['style-loader','css-loader', 'sass-loader']
         },
        ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template:'./src/template.html'
+    })]
 }
